41733c5aab50f610af7b9b5e431b0bc4c1ee8b1f
sriharsha-a <sriharsha.allagadda@stixis.com>
2018-11-17 17:37:39 -0800

    build error fix

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-var port = process.env.PORT || 4000;
 
 app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
 app.get('/*', function(req,res){
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-app.listen(port, () => {
-    console.log("App is running on port " + port);
-});
-
-
 
