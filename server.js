0abef33d9089a48689060da4dbc3714a14f7088a
sriharsha-a <sriharsha.allagadda@stixis.com>
2018-11-17 17:46:43 -0800

    build error fix

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'dist', 'index.html');
+    res.sendFile(index);
 });
+  
 
 app.listen(process.env.PORT || 8080);
 
